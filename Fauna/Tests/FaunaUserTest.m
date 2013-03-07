b23c2b712fac3abbeb0b1f1c3acf7c6edddfe8b1
Matt Freels <matt@freels.name>
2013-03-06 17:12:10 -0800

    stub test

---
+++ Fauna/Tests/FaunaUserTest.m
@@ -0,0 +1,20 @@
+//
+//  FaunaUserTest.m
+//  Fauna
+//
+//  Created by Matt Freels on 3/6/13.
+//  Copyright (c) 2013 Fauna. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+
+@interface FaunaUserTest : GHTestCase { }
+@end
+
+@implementation FaunaUserTest
+
+- (void)testAuthentication {
+  GHFail(@"pending");
+}
+
+@end
