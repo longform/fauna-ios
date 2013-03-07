bfb650e57929b8fc48215a44a97e3993b8b9c77f
Matt Freels <matt@freels.name>
2013-03-06 16:05:26 -0800

    add GHUnit and Tests target to Fauna.xc

---
+++ Fauna/Tests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tests
+//
+//  Created by Matt Freels on 3/6/13.
+//  Copyright (c) 2013 Fauna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
