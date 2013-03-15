bad53a073c657e482d8869ae487b3ccb7073e633
Matt Freels <matt@freels.name>
2013-03-15 11:47:29 -0700

    instance test stub

---
+++ Tests/FNInstanceTest.m
@@ -0,0 +1,22 @@
+//
+//  FNInstanceTest.m
+//  Fauna
+//
+//  Created by Matt Freels on 3/15/13.
+//  Copyright (c) 2013 Fauna. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+#import <Fauna/Fauna.h>
+#import "FaunaCredentials.h"
+
+@interface FNInstanceTest : GHAsyncTestCase { }
+@end
+
+@implementation FNInstanceTest
+
+- (void)testCreate {
+  GHFail(@"pending");
+}
+
+@end
