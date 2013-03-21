edbc605cc3a65453cb3762d1d72b85bb0b81168a
Matt Freels <matt@freels.name>
2013-03-21 12:16:04 -0700

    Change file headers to reflect MPL 2.0 license.

--- Fauna/Cache/FNCache.h
+++ Fauna/Cache/FNCache.h
@@ -1,9 +1,18 @@
 //
-//  FNCache.h
-//  Fauna
+// FNCache.h
 //
-//  Created by Edward Ceaser on 3/20/13.
-//  Copyright (c) 2013 Fauna. All rights reserved.
+// Copyright (c) 2013 Fauna, Inc.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain a
+// copy of the License at
+//
+// http://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
 //
 
 #import <Foundation/Foundation.h>
